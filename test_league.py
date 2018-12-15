e1ab309e097f3eba8b70c58df4b6858c07a85f71
bestick <bestick@users.noreply.github.com>
2018-12-15 13:52:22 +0300

    Первый парсинг всей таблицы без матчей

--- test_league.py
+++ test_league.py
@@ -3,7 +3,6 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 # from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import NoSuchElementException
 import unittest
-
 import time
 
 
@@ -25,15 +24,23 @@ class test_league(unittest.TestCase):
         wd = self.wd
         self.open_page('https://www.myscore.com.ua/football/russia/premier-league-2017-2018')
         while self.is_link_text_present('Показать больше матчей'):
-            time.sleep(2)
+            time.sleep(1)
 
-        tr_s = wd.find_element_by_class_name('soccer').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+        tables = wd.find_elements_by_class_name('soccer')
 
+        for ij in range(len(tables)):
+            tbody = tables[ij].find_element_by_tag_name('tbody')
+            self.tour_data(tbody)
 
+        # self.open_page('https://www.myscore.com.ua/match/UDoUtqRL/#match-summary')
+        # time.sleep(3)
+
+    def tour_data(self, tbody):
+        tr_s = tbody.find_elements_by_tag_name('tr')
         for ij in range(len(tr_s)):
-
             class_name = tr_s[ij].get_attribute('class')
-            if class_name in ['odd stage-finished', 'even stage-finished', 'even no-border-bottom stage-finished', 'odd no-border-bottom stage-finished']:
+
+            if class_name[-14:] == 'stage-finished':
                 self.match_data(tr_s[ij])
             elif class_name == 'event_round':
                 print(' ')
@@ -42,21 +49,19 @@ class test_league(unittest.TestCase):
             else:
                 print('Паршивая ситуация')
 
-
-
-
     def match_data(self, tr):
-
         print('')
         print('===========')
         print('Имя класса tr:', tr.get_attribute('class'))
-        print('id матча:', tr.get_attribute('id'))
+        id = tr.get_attribute('id')
+        print('id матча:', id)
         td_s = tr.find_elements_by_tag_name('td')
 
         print('Начало в:', td_s[1].text)
         print('Team home:', td_s[2].find_element_by_tag_name('span').text)
         print('Team away:', td_s[3].find_element_by_tag_name('span').text)
-        print('Счет:', td_s[4].text)
+        print('Счет: ', td_s[4].text)
+        print('Url на матч: ', id[4:])
 
     def open_page(self, url):
         wd = self.wd
