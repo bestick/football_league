2d24127229f615b7a765907f1ba9192937e5f43e
bestick <bestick@users.noreply.github.com>
2019-03-01 08:46:15 +0300

    Солидная переделка под BeautifulSoup
    Поменялась философия парсинга после просмотра Олега Молчанова по парсингу

--- test_league.py
+++ test_league.py
@@ -2,7 +2,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException
 import unittest
-from bs4 import BeautifulSoup
 import time
 
 
@@ -48,26 +47,71 @@ class test_league(unittest.TestCase):
 
 
     def test_match(self):
+        import requests
+        from bs4 import BeautifulSoup
+        url = 'https://www.myscore.com.ua/match/8rkygV3B/#match-summary'
+        # r = requests.get(url).text
 
+        # div = soup.find('div', class_ = 'detailMS')
+        # print('zzz :==', soup)
         wd = self.wd
         # we = self.we
-        zzz = {'Финал': [{'id': 'g_1_2iFXsPBF', 'tour': 'Финал', 'time': '20.05. 21:00', 'home': 'Анжи', 'away': 'Енисей ', 'score': '4 : 3', 'url': 'https://www.myscore.com.ua/match/2iFXsPBF/#match-summary'}, {'id': 'g_1_UDoUtqRL', 'tour': 'Финал', 'time': '20.05. 18:00', 'home': 'Тамбов', 'away': 'Амкар ', 'score': '0 : 1', 'url': 'https://www.myscore.com.ua/match/UDoUtqRL/#match-summary'}, {'id': 'g_1_O0GTr5d9', 'tour': 'Финал', 'time': '17.05. 17:30', 'home': 'Амкар', 'away': 'Тамбов', 'score': '2 : 0', 'url': 'https://www.myscore.com.ua/match/O0GTr5d9/#match-summary'}, {'id': 'g_1_UwHPqos3', 'tour': 'Финал', 'time': '17.05. 15:00', 'home': 'Енисей', 'away': 'Анжи', 'score': '3 : 0', 'url': 'https://www.myscore.com.ua/match/UwHPqos3/#match-summary'}]}
-        url = zzz['Финал'][0]['url']
+        # zzz = {'Финал': [{'id': 'g_1_2iFXsPBF', 'tour': 'Финал', 'time': '20.05. 21:00', 'home': 'Анжи', 'away': 'Енисей ', 'score': '4 : 3', 'url': 'https://www.myscore.com.ua/match/2iFXsPBF/#match-summary'}, {'id': 'g_1_UDoUtqRL', 'tour': 'Финал', 'time': '20.05. 18:00', 'home': 'Тамбов', 'away': 'Амкар ', 'score': '0 : 1', 'url': 'https://www.myscore.com.ua/match/UDoUtqRL/#match-summary'}, {'id': 'g_1_O0GTr5d9', 'tour': 'Финал', 'time': '17.05. 17:30', 'home': 'Амкар', 'away': 'Тамбов', 'score': '2 : 0', 'url': 'https://www.myscore.com.ua/match/O0GTr5d9/#match-summary'}, {'id': 'g_1_UwHPqos3', 'tour': 'Финал', 'time': '17.05. 15:00', 'home': 'Енисей', 'away': 'Анжи', 'score': '3 : 0', 'url': 'https://www.myscore.com.ua/match/UwHPqos3/#match-summary'}]}
+        # url = zzz['Финал'][0]['url']
         self.open_page(url)
         time.sleep(2)
-        info = wd.find_elements_by_class_name('detailMS')
+        # r = wd.find_element_by_id('detailMS')
+        r = wd.find_elements_by_id('summary-content')
+        print('type(r)', type(r), 'len(r)', len(r))
+
+        ttt = r[0].get_attribute('innerHTML')
+        wd.close()
+        # print('ttt:== ', ttt)
+        #
+        divs = BeautifulSoup(ttt, 'lxml').find_all('div')
+        len_divs = len(divs)
+        print( 'type(divs)', type(divs), 'len(divs)', len_divs, 'dir(divs)', dir(divs))
+        name_class = divs[1].get('class')
+        print('1:==', name_class)
+        for ij in range(len_divs - 1, 0, -1):
+
+            name_class = divs[ij].get('class')
+            print(ij, name_class)
+
+        for ij in range(len_divs - 1, 0, -1):
+            name_class = divs[ij].get('class')
+            if name_class == None:
+                print(ij, 'ura!!!')
+
+        zzz = divs[66].get('class')
+        print('type(zzz)', type(zzz), zzz, 'len(zzz)', len(zzz))
+
+
+
+        #     try:
+        #         name_class = divs[ij].get('class')
+        #         print(name_class)
+        #         if type(name_class) != list or name_class[0][:18] == 'detailMS__incident':
+        #             del divs[ij]
+        #     except:
+        #         del divs[ij]
+        # print('len(divs)', len(divs))
+
         # info = wd.find_elements_by_class_name('stage-12')
 
-        len_info = len(info)
-        print('len_info', len_info)
-        ttt = info[0].get_attribute('innerHTML')
+        # len_info = len(info)
+        # print('len_info', len_info)
+        # ttt = info[0].get_attribute('innerHTML')
+        #
+        # print('info: ==', info)
 
-        print('info: ==',  info)
-        print('ttt: ==',  ttt)
-        soup = BeautifulSoup(ttt, 'html.parser')
-        p1_away = soup.find('span', class_= 'p1_away')
-        p2_away = soup.find('span', class_='p2_away')
-        print('p1_away: ==', p1_away.text, 'p2_away: ==', p2_away.text)
+        # soup = BeautifulSoup(ttt, 'html.parser')
+        # p1_away = soup.find('span', class_= 'p1_away')
+        # p2_away = soup.find('span', class_= 'p2_away')
+        # print('p1_away: ==', p1_away.text, 'p2_away: ==', p2_away.text)
+        #
+        # div = soup.find('div')
+        # print('Type div:== ', 'div=', div)
 
         # kkk = ttt.find_elements_by_class_name('detailMS__incidentsHeader stage-12')
         #
